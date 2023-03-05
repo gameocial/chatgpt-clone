2ce43647eb1a098d85b8f37da992fd4aa152537b
gameocial <127005795+gameocial@users.noreply.github.com>
2023-03-05 11:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import gradio as gr
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #if you have OpenAI API key as a string, enable the below
-openai.api_key = "xxxxxx"
+openai.api_key = "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"
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
